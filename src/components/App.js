b1f6baf75655a58160bc15e5460c5561e2bc030f
Ingrid Amaral <guid_95@hotmail.com>
2020-02-25 12:49:56 +0100

    import streams to app and add route to each one

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow'; 
+import StreamList from './streams/StreamList';
 
 
 
 class App extends React.Component {
-    PageOne = () => {
-        return (<div>Page One
-            <Link to='/pagetwo'>Navitage to page two</Link>
-        </div>)
-    };
-    
-    PageTwo = () => {
-        return  (<div>Page Two
-            <Link to='/'>Page one</Link>
-        </div>)
-    };
-    
     
     render () {
         return (
             <div>
                 <BrowserRouter>
                     <div>
-                        <Route path='/' exact component={this.PageOne} />
-                        <Route path='/pagetwo' exact component={this.PageTwo} />
+                        <Route path='/' exact component={StreamList} />
+                        <Route path='/streams/new' exact component={StreamCreate} />
+                        <Route path='/streams/edit' exact component={StreamEdit} />
+                        <Route path='/streams/delete' exact component={StreamDelete} />
+                        <Route path='/streams/show' exact component={StreamShow} />
                     </div>
                 </BrowserRouter>
             </div>
