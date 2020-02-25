d95ba46bc8c7b6aa68a2f5827c0555e79ddcd015
Ingrid Amaral <guid_95@hotmail.com>
2020-02-25 15:50:45 +0100

    on demand sign in and sign out

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -22,19 +22,27 @@ class GoogleAuth extends React.Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() })
     }
 
+    handleSignIn = () => {
+        this.auth.signIn();
+    }
+
+    handleSignOut = () => {
+        this.auth.signOut();
+    }
+
     renderAuthButton = () => {
         if (this.state.isSignedIn === null) {
             return null
         } else if (this.state.isSignedIn) {
             return (
-                <button className='ui red google button'>
+                <button onClick={this.handleSignOut} className='ui red google button'>
                     <i className='google icon' />
                     Sign Out
                 </button>
             )
         } else {
             return (
-                <button className='ui red google button'>
+                <button onClick={this.handleSignIn} className='ui red google button'>
                     <i className='google icon' />
                     Sign In With Google
                 </button>
