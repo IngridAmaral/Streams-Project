4d78c8f13f5de63e6a522e724d969e1f8414bd8a
Ingrid Amaral <guid_95@hotmail.com>
2020-02-25 17:23:08 +0100

    building auth reducer

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions'
 
 class GoogleAuth extends React.Component {
     state = {
@@ -18,8 +20,13 @@ class GoogleAuth extends React.Component {
         })
     }
 
-    handleAuthChange = (e) => {
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    handleAuthChange = (isSignedIn) => {
+        if (isSignedIn) {
+            this.props.signIn();
+        } else {
+            this.props.signOut();
+        }    
+        //this.setState({ isSignedIn: this.auth.isSignedIn.get() })
     }
 
     handleSignInClick = () => {
@@ -58,4 +65,4 @@ class GoogleAuth extends React.Component {
     }
 }
 
-export default GoogleAuth;
+export default connect(null, { signIn, signOut })(GoogleAuth);
