420c2388568ad7dfc1cd9be9e56b40ea745358df
Ingrid Amaral <guid_95@hotmail.com>
2020-02-26 12:37:07 +0100

    handle form submit

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -24,13 +24,25 @@ class StreamCeate extends React.Component {
             </div>
         ) 
     }
+
+    handleSubmit = (formValues) => {
+        //e.preventDefault(); no need to call it anymore
+        console.log(formValues)
+    }
  
     render() {
         //console.log(this.props)
         return (
-            <form className='ui form'>
-                <Field name='title' component={this.renderInput} label='Enter Title' />
-                <Field name='description' component={this.renderInput} label='Enter Description' />
+            <form onSubmit={this.props.handleSubmit(this.handleSubmit)} className='ui form'>
+                <Field 
+                    name='title' 
+                    component={this.renderInput} 
+                    label='Enter Title' />
+                <Field 
+                    name='description' 
+                    component={this.renderInput} 
+                    label='Enter Description' />
+                <button className='ui button primary'>Submit</button>
             </form>
         )
     }
