3401786d605a407e19a153f7e68f123dae4e05f5
Ingrid Amaral <guid_95@hotmail.com>
2020-02-26 18:01:58 +0100

    merging lists of records

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from  'redux-form'; //rename the reducer to formReducer
 import authReducer from './authReducer';
+import streamsReducer from './streamReducer';
 
 export default combineReducers({
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    streams: streamsReducer
 });
