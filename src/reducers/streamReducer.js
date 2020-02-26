7251725ed14b3b8a0a840d71a4d96371da696a45
Ingrid Amaral <guid_95@hotmail.com>
2020-02-26 17:09:04 +0100

    deletind properties with _.omit

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,38 @@
+import _ from  'lodash'
+
+import { 
+    EDIT_STREAM,
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    DELETE_STREAM,
+    CREATE_STREAM
+} from "../actions/types";
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case FETCH_STREAM:
+            return { ...state, [action.payload.d]: action.payload };
+        case CREATE_STREAM:
+            return { ...state, [action.payload.d]: action.payload };   
+        case EDIT_STREAM:
+            return { ...state, [action.payload.d]: action.payload };
+        case DELETE_STREAM:
+            return _.omit(state, action.payload)  //in this case the payload is the id itself, omit creates a new state object without whatever is passed
+        default:
+            return state
+    }
+}
+
+// const streamReducer = { state = {}, action} => {
+//     switch (action.type) {
+//         case EDIT_STREAM:
+//             // const newState = { ...state }
+//             // newState[action.payload.id] = action.payli
+//             // return newState 
+//             // OR
+
+//             return { ...state, [action.payload.id]: action.payload }
+//         default:
+//             return state
+//     }
+// }
