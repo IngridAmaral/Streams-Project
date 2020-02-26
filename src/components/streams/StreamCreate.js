0f487830faad973d68d837ad3e05ec48afa58031
Ingrid Amaral <guid_95@hotmail.com>
2020-02-26 12:25:30 +0100

    customizingform fields

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,27 +2,35 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCeate extends React.Component {
-    //renderInput = (formProps) => {
-    renderInput = ({ input }) => { //destructured
-        //console.log(formProps)
         
-        // return (
-        //     <input 
-        //         onChange={formProps.input.onChange}
-        //         value={formProps.input.value} 
-        //     />
-        // )  NEW SINTAX BELLOW
-
-        //return <input {...formProps.input} />
-        return <input {...input} /> //take all the key value pairs and add them as properties to the input element
+    //   renderInput = (formProps) => {
+    //         console.log(formProps)
+            
+    //         return (
+    //             <input 
+    //                 onChange={formProps.input.onChange}
+    //                 value={formProps.input.value} 
+    //             />
+    //         )  
+    
+    //         return <input {...formProps.input} /> //take all the key value pairs and add them as properties to the input element
+    //     } NEW SINTAX BELLOW
+    
+    renderInput = ({ input, label }) => { //destructured
+        return (
+            <div className='field'>
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        ) 
     }
  
     render() {
         //console.log(this.props)
         return (
-            <form>
-                <Field name='title' component={this.renderInput} />
-                <Field name='description' component={this.renderInput} />
+            <form className='ui form'>
+                <Field name='title' component={this.renderInput} label='Enter Title' />
+                <Field name='description' component={this.renderInput} label='Enter Description' />
             </form>
         )
     }
