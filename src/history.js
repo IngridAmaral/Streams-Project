db8b931509cfb5804fcd4411d1ade37e8c4a3224
Ingrid Amaral <guid_95@hotmail.com>
2020-02-28 17:18:50 +0100

    implementing programmatic navigation

---
+++ src/history.js
@@ -0,0 +1,5 @@
+import createHistory from 'history/createBrowserHistory';
+ 
+export default createHistory(); 
+
+//creates a historu object that is maintained and can be acessed anywhere inside te project
