eade3888fc61ad37f13d7c0a08f249c48cdab75f
Ingrid Amaral <guid_95@hotmail.com>
2020-03-02 17:43:15 +0100

    avoiding changes to properties

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 import { fetchStream, editStream } from '../../actions';
 import StreamForm from './StreamForm'
@@ -24,7 +25,7 @@ class StreamEdit extends React.Component {
             <div>
                 <h3>Edit a Stream</h3>
                 <StreamForm 
-                    initialValues={this.props.stream} 
+                    initialValues={_.pick(this.props.stream, 'title', 'description')} 
                     submitProp={this.handleSubmit} />
             </div>
         )
