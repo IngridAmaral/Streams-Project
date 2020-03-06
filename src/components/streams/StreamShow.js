665e0fceae0b533cf91869f7eadf0e9c5a9f1083
Ingrid Amaral <guid_95@hotmail.com>
2020-03-06 14:15:05 +0100

    showing a stream

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -8,14 +8,25 @@ class StreamShow extends React.Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id)
     }
-    
+
     render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>;
+        }
+
+        const { title, description } = this.props.stream
+        
         return (
             <div>
-                Stream Show
+                <h1>{title}</h1>
+                <h5>{description}</h5>
             </div>
         )
     }
 }
 
-export default connect(null, { fetchStream })(StreamShow);
+const mapStateToProps = (state, onwProps) => {
+    return { stream: state.streams[onwProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
