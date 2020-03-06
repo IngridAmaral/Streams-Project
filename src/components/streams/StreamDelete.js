cc8914daca68d8a97e430007d939038ff7eadc5f
Ingrid Amaral <guid_95@hotmail.com>
2020-03-06 12:45:59 +0100

    conditionally showing stream details

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -7,10 +7,10 @@ import { fetchStream, deleteStream } from '../../actions';
 
 class StreamDelete extends React.Component {
     componentDidMount() {
-        console.log(this.props)
+        //console.log(this.props)
         this.props.fetchStream(this.props.match.params.id)
     }
-    
+
     // handleDelete = () => {
     //     this.props.deleteStream(this.props.match.params.id)
     // }
@@ -24,25 +24,30 @@ class StreamDelete extends React.Component {
         )
     }
 
+    renderContent = () => {
+        if (!this.props.stream) {
+            return 'Are you sure you want to delete this stream?'
+        }
+        return `Are you sure you want to delete the stream with title: ${this.props.stream.title}`
+    }
+
     render() {
+        console.log(this.props.match.params.id)
         return (
-            <div>
-                Stream Delete
-                <Modal
-                    title='Delete Stream'
-                    content='Are you sure you want to delete this stream?'
-                    actions={this.renderActions()}
-                    onDismiss={() => history.push('/')} />
-            </div>
+            <Modal
+                title='Delete Stream'
+                content={this.renderContent()}
+                actions={this.renderActions()}
+                onDismiss={() => history.push('/')} />
         )
     }
 }
 
 
 
-// const mapStateToProps = (state, onwProps) => {
-//     console.log(state, onwProps)
-//     return { stream: state.streams[onwProps.match.params.id]}
-// }
+const mapStateToProps = (state, onwProps) => {
+    console.log(state, onwProps)
+    return { stream: state.streams[onwProps.match.params.id] }
+}
 
-export default connect(null, {fetchStream, deleteStream})(StreamDelete);
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
